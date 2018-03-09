23d048ec44ac267aa30a2154a7fdf1bdb2a5dd2a
Krish44 <emailstokp@gmail.com>
2018-03-09 17:17:12 +0530

    Initial edit

---
+++ Notes/ListOps.py
@@ -0,0 +1,5 @@
+Colors = ["Red", "Orange", "Yellow", "Green", "Blue"]
+print(*Colors, sep='\n')
+for Item in Colors: print(Item.rjust(8), sep='/n')
+print'\n'.join(Colors))
+print('First: {0}\nSecond: {1'.format(*Colors))
