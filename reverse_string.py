e65a0aee06f8a6db5f257d9076d0c5b5735280b5
Krish44 <emailstokp@gmail.com>
2021-04-07 18:59:03 +0530

    Create reverse_string.py

---
+++ reverse_string.py
@@ -0,0 +1,25 @@
+#
+# Reverse a given string and see if it is a palindrome
+# Print the result if its a palindrome
+#
+
+def check_palindrome(in_list):
+    for v_index, v_value in enumerate(in_list):
+        v_value = v_value.lower()
+        # if v_value == v_value[::-1]:
+        # [OR] use below condition
+        if v_value == "".join(reversed(v_value)):
+            print("Index:",v_index, "Value:", v_value)
+
+
+my_list = ['string1','pop','Malayalam','nitin','spoon']
+check_palindrome(my_list)
+
+
+"""
+OutPut:
+-------
+Index: 1 Value: pop
+Index: 2 Value: malayalam
+Index: 3 Value: nitin
+"""
