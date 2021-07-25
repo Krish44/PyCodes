93126799561f521e17cacb7f04fe842c412902f4
Krish44 <emailstokp@gmail.com>
2021-07-25 18:59:02 +0530

    Update manual_sort_2.py
    Added output data

--- manual_sort_2.py
+++ manual_sort_2.py
@@ -17,3 +17,24 @@ print("reverse via slicing:", list_1[::-1])
 print("reversed via function:", [data for data in reversed(list_1)])
 list_1.reverse()
 print("Reversed Inline:", list_1)
+
+
+"""
+Output: 
+[2, 9, 66, 5, 3, 1]
+Intermediary moves: [2, 9, 5, 66, 3, 1]
+Intermediary moves: [2, 9, 5, 3, 66, 1]
+Intermediary moves: [2, 9, 5, 3, 1, 66]
+Intermediary moves: [2, 5, 9, 3, 1, 66]
+Intermediary moves: [2, 5, 3, 9, 1, 66]
+Intermediary moves: [2, 5, 3, 1, 9, 66]
+Intermediary moves: [2, 3, 5, 1, 9, 66]
+Intermediary moves: [2, 3, 1, 5, 9, 66]
+Intermediary moves: [2, 1, 3, 5, 9, 66]
+Intermediary moves: [1, 2, 3, 5, 9, 66]
+Final Sorted: [1, 2, 3, 5, 9, 66]
+reverse via slicing: [66, 9, 5, 3, 2, 1]
+reversed via function: [66, 9, 5, 3, 2, 1]
+Reversed Inline: [66, 9, 5, 3, 2, 1]
+
+"""
