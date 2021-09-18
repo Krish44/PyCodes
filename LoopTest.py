3891b05ffa82d1cf50c4eb05f12b8b7875c1c216
kp <mail@kp.com>
2021-09-18 17:41:14 +0530

    Folder formatting

--- LoopTest.py
+++
@@ -1,5 +0,0 @@
-
-my_list = [1,5,8]
-for i in my_list:
-    #print(i)
-    print('Data Set', i)
--- StringsFuncs.py
+++
@@ -1,9 +0,0 @@
-import string
-import random
-
-print(dir(string))  # will list all available methods
-print("Prints ASCII letters: ", string.ascii_letters)
-StringSet = string.ascii_lowercase
-print("ASCII lower: ", StringSet)
-
-print(random.choice(StringSet))
