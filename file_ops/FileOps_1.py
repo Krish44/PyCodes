d8912a19637f088084d19b3c58df960428b409aa
Krish44 <emailstokp@gmail.com>
2022-02-21 09:32:45 +0530

    Create FileOps_1.py

---
+++ file_ops/FileOps_1.py
@@ -0,0 +1,24 @@
+"""
+Requirement:
+    Read space separated file.
+    Product quantity from 3rd row.
+    Replace E103 with E111.
+    Write unique user name and ID to new file
+
+In file Sample:
+User ID Product Strore
+Tony E101 pencil CA
+Rony E102 pencil DA
+Tom E103 flip-flop CA
+Tom E103 hatcher DA
+Young E105 mattress GR
+Jack E107 gun BR
+"""
+
+# Read space separated file
+in_file = open('/home/krishnap/workbench/testDir/storeFile.txt','r')
+in_file.rea
+# Product quantity from 3rd row.
+# Replace E103 with E111.
+# Write unique user name and ID to new file
+
